a4eea2174bc2c4c1d126583f2e27b7a37b774375
freddie784 <83510059+freddie784@users.noreply.github.com>
2021-10-17 18:41:30 -0400

    created summary statistics for the suspension coil data

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,5 +4,8 @@ MechaCar_mpg <- read_csv("MechaCar_mpg.csv")
 Suspension_Coil <- read_csv("Suspension_Coil.csv")
 
 model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, MechaCar_mpg)
-
 summary(model)
+
+totalSum <- Suspension_Coil  %>% summarize(mean(PSI),median(PSI),var(PSI),sd(PSI), .groups = 'keep') #create a summary table
+lotSummary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(mean(PSI),median(PSI),var(PSI),sd(PSI), .groups = 'keep') #create a summary table
+
